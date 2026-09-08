6e2acaf321523c801e4e0b97190ae45b8bffdf63
Piplot5x <a20204007@pucp.edu.pe>
2026-09-08 15:56:07 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+fetch("../../components/header.html")
+    .then(response => response.text())
+    .then(data => {
+        console.log(data);
+        document.querySelector("#header").innerHTML = data;
+    });
+
+fetch("../../components/footer.html")
+    .then(response => response.text())
+    .then(data => {
+        document.querySelector("#footer").innerHTML = data;
+    });
